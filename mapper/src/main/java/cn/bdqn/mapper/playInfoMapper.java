aaef1edd1b5a8ca01a86227b1039d8dc666b9980
XingChen <l3044579470@163.com>
2020-09-29 17:42:14 +0800

    修改了playInfo中的Mapper接口，以及Mapper.xml

--- mapper/src/main/java/cn/bdqn/mapper/playInfoMapper.java
+++ mapper/src/main/java/cn/bdqn/mapper/playInfoMapper.java
@@ -2,6 +2,7 @@ package cn.bdqn.mapper;
 
 import cn.bdqn.domain.playInfo;
 import cn.bdqn.domain.ticketInfo;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -13,6 +14,16 @@ public interface playInfoMapper {
          Integer playInfoInsert(playInfo playInfo);
          //更新更多电影放映信息
         Integer playInfoUpdate(playInfo playInfo);
-        //查询以电影放映信息查询票数
-        List<ticketInfo> selectTicketPlayID(Integer playID);
+        //查询电影影院ID,当前时间，结束时间；
+        List<playInfo> selectFilmID(@Param("filmID") Integer filmID, @Param("dangDateString") String dangDateString, @Param("houDateString") String houDateString);
+        //查询电影D,当前时间，结束时间；
+        List<playInfo> selectCinemaID( @Param("cinemaID") Integer cinemaID,@Param("dangDateString") String dangDateString,@Param("houDateString") String houDateString);
+
+         Integer selectCountAll(List<String> sqlList, String sqlstring);
+        List<playInfo> selectPlayInfoPage(List<String> sqlList, String sqlstring, Integer pageSize, Integer sqlCount);
+
+        playInfo selectPlayID(Integer playID);
+
+        List<Integer> selectPlayIDdelete(Integer ID, String sQLstring);
+
 }
