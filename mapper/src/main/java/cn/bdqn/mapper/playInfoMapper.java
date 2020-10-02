ff205d3ec1a10b799b8edb10b48e522f74eea63f
XingChen <l3044579470@163.com>
2020-10-02 16:10:03 +0800

    写了playinfo的查询及增删改接口方法，及playinfoMapper.xml

--- mapper/src/main/java/cn/bdqn/mapper/playInfoMapper.java
+++ mapper/src/main/java/cn/bdqn/mapper/playInfoMapper.java
@@ -14,6 +14,13 @@ public interface playInfoMapper {
          Integer playInfoInsert(playInfo playInfo);
          //更新更多电影放映信息
         Integer playInfoUpdate(playInfo playInfo);
+        //根据电影放映信息id删除
+        Integer playinfoDelete(Integer playID);
+
+        //根据影片删除Id
+        Integer filminfoDelete(Integer filmID);
+        //根据id删除电影
+        Integer   deleteCinema(Integer cinemaID);
         //查询电影影院ID,当前时间，结束时间；
         List<playInfo> selectFilmID(@Param("filmID") Integer filmID, @Param("dangDateString") String dangDateString, @Param("houDateString") String houDateString);
         //查询电影D,当前时间，结束时间；
@@ -21,7 +28,7 @@ public interface playInfoMapper {
 
          Integer selectCountAll(List<String> sqlList, String sqlstring);
         List<playInfo> selectPlayInfoPage(List<String> sqlList, String sqlstring, Integer pageSize, Integer sqlCount);
-
+        //根据电影放映信息表Id查询
         playInfo selectPlayID(Integer playID);
 
         List<Integer> selectPlayIDdelete(Integer ID, String sQLstring);
